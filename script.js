bfc3b3919b1a14c3af52bc0f96c8e5d37a2ae7ab
Kamil <kamilbialek2k05@gmail.com>
2022-12-02 13:32:59 +0100

    reference to container and loop creating new div and append them as that container childs same with those childs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const container = document.querySelector('.container');
+//horizontal grid
+//vertical grid
+const container = document.querySelector('#container');
+for (i=0; i<16; i++){
+    const vertical_grid = document.createElement('div');
+    vertical_grid.classList.add('vertical_grid'); 
+    container.appendChild(vertical_grid); 
+}
+const vertical_grid= document.querySelectorAll('.vertical_grid');
+for (i=0; i<16; i++){
+    const horizontal_grid = document.createElement('div');
+    horizontal_grid.classList.add('horizontal_grid');
+    vertical_grid.appendChild(horizontal_grid);
+}

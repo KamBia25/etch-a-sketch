6ded6b0d5db8632a98734537e369711c9bf021ff
Kamil <kamilbialek2k05@gmail.com>
2022-12-02 13:57:22 +0100

    made square grid of divs 16x16

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-//horizontal grid
-//vertical grid
 const container = document.querySelector('#container');
-for (i=0; i<16; i++){
-    const vertical_grid = document.createElement('div');
-    vertical_grid.classList.add('vertical_grid'); 
-    container.appendChild(vertical_grid); 
+for (let i=1; i<=16; i++){
+    const verticalGrid = document.createElement('div');
+    verticalGrid.classList.add(`verticalGrid-${i}`); 
+    container.appendChild(verticalGrid); 
+    
+for (let j=0; j<16; j++){
+    let gridFoundations= document.querySelector(`.verticalGrid-${i}`);
+    const horizontalGrid = document.createElement('div');
+    horizontalGrid.classList.add('horizontalGrid');
+    gridFoundations.appendChild(horizontalGrid);
 }
-const vertical_grid= document.querySelectorAll('.vertical_grid');
-for (i=0; i<16; i++){
-    const horizontal_grid = document.createElement('div');
-    horizontal_grid.classList.add('horizontal_grid');
-    vertical_grid.appendChild(horizontal_grid);
 }

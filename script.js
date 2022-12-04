cf448ffb19e521215ce80a568c7bd7e2b43d7e7f
Kamil <kamilbialek2k05@gmail.com>
2022-12-04 10:21:35 +0100

    switch from classes to ids

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('#container');
 for (let i=1; i<=16; i++){
     const verticalGrid = document.createElement('div');
-    verticalGrid.classList.add(`verticalGrid-${i}`); 
+    verticalGrid.setAttribute('id',`verticalGrid-${i}`); 
     container.appendChild(verticalGrid); 
     
 for (let j=0; j<16; j++){
-    let gridFoundations= document.querySelector(`.verticalGrid-${i}`);
+    let gridFoundations= document.querySelector(`#verticalGrid-${i}`);
     const horizontalGrid = document.createElement('div');
     horizontalGrid.classList.add('horizontalGrid');
     gridFoundations.appendChild(horizontalGrid);

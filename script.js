e9f20d9de924c98921a74727ba5bd56cc337e330
Kamil <kamilbialek2k05@gmail.com>
2023-02-15 15:13:34 +0100

    value of slider shows below in the paragraph

--- script.js
+++ script.js
@@ -40,4 +40,9 @@ const span = document.createElement('span')
 span.setAttribute('id','numeralValue')
 thatValue.append(span)
 
-
+var slider = document.getElementById("myRange");
+var output = document.getElementById("numeralValue");
+output.innerHTML = slider.value;
+slider.oninput = function() {
+    output.innerHTML = this.value;
+  }

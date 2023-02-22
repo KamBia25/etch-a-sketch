484041c5e17726ff6a0cf5776cb58642de5a0c9f
Kamil <kamilbialek2k05@gmail.com>
2023-02-22 15:35:12 +0100

    color input changes which color paints the grid

--- script.js
+++ script.js
@@ -8,22 +8,25 @@ function deleteChild() {
 }
 
 const headerBody = document.querySelector('body');
-const header = document.createElement("header");
-headerBody.appendChild(header);
+  const header = document.createElement("header");
+  headerBody.appendChild(header);
+
 const headerText = document.querySelector('header');
-const firstHeader = document.createElement("h1");
-firstHeader.textContent = "Etch-a-Sketch";
-headerText.appendChild(firstHeader);
+  const firstHeader = document.createElement("h1");
+  firstHeader.textContent = "Etch-a-Sketch";
+  headerText.appendChild(firstHeader);
+
 
 const addMain = document.querySelector('body');
-const mainDiv = document.createElement('div');
-mainDiv.setAttribute('id','main');
-addMain.appendChild(mainDiv);
+  const mainDiv = document.createElement('div');
+  mainDiv.setAttribute('id','main');
+  addMain.appendChild(mainDiv);
 
 const main = document.querySelector('#main');
-const mainContainer = document.createElement('div');
-mainContainer.setAttribute('id','container');
-main.appendChild(mainContainer);
+  const mainContainer = document.createElement('div');
+  mainContainer.setAttribute('id','container');
+  main.appendChild(mainContainer);
+
 
 var container = document.querySelector('#container');
   for (let i=1; i<=16; i++){
@@ -39,40 +42,47 @@ for (let j=0; j<16; j++){
     gridFoundations.appendChild(horizontalGrid);
 }
 }  
-const paintBrush = document.querySelectorAll('.horizontalGrid');
-paintBrush.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-    div.classList.add('color'); 
-  });
-});
+// const paintBrush = document.querySelectorAll('.horizontalGrid');
+// paintBrush.forEach((div) => {
+//     div.addEventListener('mouseover', () => {
+//     div.classList.add('color'); 
+//   });
+// });
 
 
 const body = document.querySelector('#main');
-const rangeSlider = document.createElement('input');
-rangeSlider.setAttribute('id',`myRange`);
-rangeSlider.classList.add('slider'); 
-rangeSlider.setAttribute(`max`,`64`);
-rangeSlider.setAttribute(`min`,`8`);
-rangeSlider.setAttribute(`value`,`16`);
-rangeSlider.setAttribute(`type`,`range`);
-body.appendChild(rangeSlider);
+  const rangeSlider = document.createElement('input');
+  rangeSlider.setAttribute('id',`myRange`);
+  rangeSlider.classList.add('slider'); 
+  rangeSlider.setAttribute(`max`,`64`);
+  rangeSlider.setAttribute(`min`,`8`);
+  rangeSlider.setAttribute(`value`,`16`);
+  rangeSlider.setAttribute(`type`,`range`);
+  body.appendChild(rangeSlider);
 
 const value = document.createElement('p');
-value.setAttribute('id','text');
-value.textContent = "Value: ";
-body.appendChild(value);
+  value.setAttribute('id','text');
+  value.textContent = "Value: ";
+  body.appendChild(value);
 
 const thatValue = document.querySelector('#text');
-const span = document.createElement('span')
-span.setAttribute('id','numeralValue')
-thatValue.append(span)
+  const span = document.createElement('span');
+  span.setAttribute('id','numeralValue');
+  thatValue.append(span)
+
+const color = document.querySelector('#main');
+  const colorChange = document.createElement('input');
+  colorChange.setAttribute('type','color');
+  colorChange.setAttribute('id','colorChange')
+  color.appendChild(colorChange);
+
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("numeralValue");
-output.innerHTML = slider.value;
-slider.oninput = function() {
+  var output = document.getElementById("numeralValue");
+  output.innerHTML = slider.value;
+  slider.oninput = function() {
     output.innerHTML = this.value;
-  }
+    }
 
 
 slider.onchange = function() {
@@ -92,14 +102,40 @@ for (let j=0; j<sizeofGrid; j++){
 
 }
 }
+// const paintBrush = document.querySelectorAll('.horizontalGrid');
+// paintBrush.forEach((div) => {
+//     div.addEventListener('mouseover', () => {
+//     div.classList.add('color'); 
+//   });
+// });
+// }
+
+let colorChange;
+const defaultColor = "#0000ff";
+window.addEventListener("load", startup, false);
+function startup() {
+  colorChange = document.querySelector("#colorChange");
+  colorChange.value = defaultColor;
+  colorChange.select();
+}
+
+}
 const paintBrush = document.querySelectorAll('.horizontalGrid');
 paintBrush.forEach((div) => {
     div.addEventListener('mouseover', () => {
-    div.classList.add('color'); 
+     div.style.backgroundColor =colorChange.value; 
   });
 });
-}
+
+
+
 
 const bodyFooter = document.querySelector('body');
-const footer = document.createElement("footer");
-bodyFooter.appendChild(footer);
+  const footer = document.createElement("footer");
+  bodyFooter.appendChild(footer);
+
+
+
+
+
+
